6771da936230fbe6ebe9355133fac8e4dd0fbbd0
T4D4 .icp <t4d4.icp@gmail.com>
2025-02-21 14:14:02 +0000

    wakatime: remove hardcoded API key for security

--- modules/nixvim/plug/wakatime/default.nix
+++ modules/nixvim/plug/wakatime/default.nix
@@ -1,14 +1,8 @@
-{ lib, ... }:
-
-let
-  cfgFilePath = "${builtins.getEnv "HOME"}/.wakatime.cfg";
-  wakatimeApiKey = builtins.match "api_key = (.*)" (builtins.readFile cfgFilePath);
-in
 {
   plugins.wakatime = {
     enable = true;
     settings = {
-      api_key = builtins.elemAt wakatimeApiKey 0;
+      api_key = "";
     };
   };
 }
