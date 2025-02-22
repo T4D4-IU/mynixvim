4b04ee18e0b0cd8f40a3a911325058c520868a49
T4D4 .icp <t4d4.icp@gmail.com>
2025-02-22 14:37:28 +0000

    add some plugins

--- config/bufferline.nix
+++ config/bufferline.nix
@@ -1,7 +1,16 @@
 {
   plugins = {
     bufferline.enable = true;
+    wakatime = {
+      enable = true;
+      autoLoad = true;
+    };
     web-devicons.enable = true;
-    colorschemes.tokyonight.enable = true;
+    which-key = {
+      enable = true;
+      autoLoad = true;
+    };
+    yazi.enable = true;
+    zen-mode.enable = true;
   };
 }
