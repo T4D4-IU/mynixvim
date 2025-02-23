4a064b7f27a1ff01f21f5e10c95c107c3deb0b42
T4D4 .icp <t4d4.icp@gmail.com>
2025-02-23 13:33:23 +0000

    gh_actions_ls、move_analyzer、solidityプラグインを追加し、設定を更新しました

--- config/plugins.nix
+++ config/plugins.nix
@@ -23,12 +23,23 @@
       servers = {
         astro.enable = true;
         biome.enable = true;
-        gh_actions_ls.enable = true;
+        gh_actions_ls = {
+          enable = true;
+          package = "null";
+        };
         graphql.enable = true;
         html.enable = true;
         htmx.enable = true;
         markdown_oxide.enable = true;
+        move_analyzer = {
+          enable = true;
+          package = "null";
+        };
         svelte.enable = true;
+        solidity = {
+          enable = true;
+          package = "null";
+        };
         tailwindcss.enable = true;
         terraform_lsp.enable = true;
         ts_ls.enable = true;
