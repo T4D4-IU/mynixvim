320be8158046fdb8d1c9c58dede74bbef4208515
T4D4 .icp <t4d4.icp@gmail.com>
2025-02-23 13:34:44 +0000

    fix setting

--- config/plugins.nix
+++ config/plugins.nix
@@ -25,7 +25,7 @@
         biome.enable = true;
         gh_actions_ls = {
           enable = true;
-          package = "null";
+          package = null;
         };
         graphql.enable = true;
         html.enable = true;
@@ -33,12 +33,12 @@
         markdown_oxide.enable = true;
         move_analyzer = {
           enable = true;
-          package = "null";
+          package = null;
         };
         svelte.enable = true;
         solidity = {
           enable = true;
-          package = "null";
+          package = null;
         };
         tailwindcss.enable = true;
         terraform_lsp.enable = true;
