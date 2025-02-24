a1fc9b06f7d36a9d124c29000affc94e021c361b
T4D4 .icp <t4d4.icp@gmail.com>
2025-02-24 14:24:06 +0000

    options.nixにタブ幅、インデント、折り返し設定を追加

--- config/options.nix
+++ config/options.nix
@@ -2,6 +2,12 @@
     opts = {
         number = true; # 行数を表示
         shiftwidth = 2; # インデント幅
+        tabstop = 2; # タブ幅
+        expandtab = true; # タブをスペースに変換
+        autoindent = true; # 自動インデント
+        smartindent = true; # スマートインデント
+        wrap = true; # 折り返し
+        linebreak = true; # 折り返し時に単語を分割
     };
     colorschemes.catppuccin.enable = true; # colorscheme
 }
